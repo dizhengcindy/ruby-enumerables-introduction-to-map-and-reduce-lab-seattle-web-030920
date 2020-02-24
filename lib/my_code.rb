f9069e5655a0229963a110d6f28958228fe564c1
Di Zheng <dizhengcindy@gmail.com>
2020-02-24 21:58:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -30,13 +30,9 @@ end
 source_array
 end
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point = 0)
   i = 0 
-  total = 0 
-  
-  if starting_point != nil
-  total += starting_point
-  end
+  total =  starting_point
   
   while i < source_array.length do 
   total += source_array[i] 
